95d1bde8cf6f2ce45b884db234c0930ef7269354
dasun <dasun@wso2.com>
2022-09-23 10:44:40 +0530

    initial comment

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
 const express = require('express')
-const app = express();
-const mongoose = require('mongoose')
 const morgan = require('morgan')
+
 require('dotenv/config')
 
+
+
+const app = express();
 app.use(morgan('tiny'));
 app.use(express.json())
+
+
+
+
+
+
+
+
 const userRouter = require('./src/routes/user')
-
-
-
-
-
-
 app.use("/users", userRouter)
-
-
-mongoose.connect(process.env.DB_URL, () => console.log("Connected To DB")).catch(err => {
-    console.log(err);
-})
-
-
 
 
 app.listen(8080)
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,38 +1,68 @@
 const express = require('express')
 const router = express.Router()
-const User = require('../models/User')
+const connection = require('../utils/db')
 
 
 
 
 router.get("/", (req, res) => {
-    User.find().then(users => {
-        res.status(200).json(users)
-    }).catch(err => {
-        console.log(err);
-        res.json({ message: err })
+    const query = 'Select * from user'
+    connection.connect((err) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            connection.query(query, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    res.send({ err })
+                }
+                if (result.lenght > 0) {
+                    res.status(200).json(result)
+                } else {
+                    res.status(400).json({ message: `Empty Database` })
+                }
+            })
+        }
+        connection.end();
     })
 
 })
 
 router.post("/new", (req, res) => {
     const { email, firstName, lastName, contactNumber } = req.body
-    const user = new User({ email, firstName, lastName, contactNumber })
 
-    user.save()
-        .then(data => {
-            res.status(201).json(data)
-        })
-        .catch(err => {
+
+})
+
+
+router.route("/:id").get((req, res) => {
+    const id = req.params.id
+    const query = `Select * from user where id = ${id}`
+    connection.connect((err) => {
+        if (err) {
             console.log(err);
-            res.json({ message: err })
-        })
+        }
+        else {
+            connection.query(query, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    res.send({ err })
+                }
+                if (result.lenght > 0) {
+                    res.status(200).json(result)
+                } else {
+                    res.status(400).json({ message: `User with ID ${id} not found` })
+                }
+            })
+        }
+        connection.end();
+    })
+
+    console.log(connection.state);
 
 })
 
-router.get("", (req, res) => {
-
-})
 
 
 module.exports = router
